3eec00021be7acf569ab35c529d3f10115151bdd
Dmitrii <dimterion59@gmail.com>
2023-05-09 23:06:54 +0200

    add more texts & options

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -5,6 +5,16 @@ import GameComponent from "../components/GameComponent";
 import gameContents from "../utils/gameContents";
 
 function GameScreen() {
+  // LOCAL STORAGE SAVE TO ADD LATER
+  // --->
+  // const [optionId, setOptionId] = useState(
+  //   localStorage.getItem("optionId") || gameContents[0].id
+  // );
+  //
+  // localStorage.setItem("optionId", optionId);
+  // <---
+  // + TO ADD: BUTTONS TO SAVE/RESET PROGRESS (SET STATE TO THE INITIAL ONE); REPLACE STATE ON THE LINE BELOW WITH THIS ONE
+
   const [optionId, setOptionId] = useState(gameContents[0].id);
 
   const contents = gameContents.map((content) => {
--- src/utils/gameContents.js
+++ src/utils/gameContents.js
@@ -33,12 +33,48 @@ const gameContents = [
     Arika looks sad, thinking about something else, as if she is somewhere in a different place, far away. Then she shakes her head abruptly, smiles and continues in a cheerful voice.
     
     “Anywho, about this place. A while ago I managed to slip by the security systems in the real world and installed a camera right where we are sitting right now.” - She giggles and winks at you. - “So, we’re kinda watching the online feed. I did some tweaking, cleared the image a bit and so on. Well, I admit, this metal piece is somewhat smaller in reality, but, what’s funny, it’s still there. Can you imagine, with all the work and planning, they forgot about it somewhere along the construction. I like to come here from time to time and watch what’s still there, beneath us. The remains of our old home, which is now no more than a foundation, slowly collapsing and fading away.”`,
-    options: [],
+    options: [
+      {
+        text: `“Why are we here?”`,
+        nextText: 4,
+      },
+      {
+        text: `“You went outside?”`,
+        nextText: 3,
+      },
+    ],
   },
   {
     id: 2,
-    subText: `“What am I, Arika? Well, I know that I’m an artificial being, created by Deo. And I guess I’m sort of self-aware… But I still don’t feel like it. I don’t see any difference between me before the old man told me the truth and me after that. It’s like I’m still human Soai, a poet that’s trying to get good and go to the higher levels.”`,
-    text: ``,
+    subText: `“What am I, Arika? Well, I know that I’m an artificial being, created by Deo. And I guess I’m sort of self-aware… But I still don’t feel like it. I don’t see any difference between me before the old man told me the truth and me after that. It’s like I’m still human Soai, a poet that’s trying to get good and climb to the higher levels.”`,
+    text: `Arika smiles.
+
+    “Isn’t that a good thing? Having doubts, being not sure. That’s what life is all about. You don’t see any difference, because there is none.” - She chuckles. - “Well, apart from the fact that you seem way less interested in winning the Writer’s Fights. But we all shift our perspectives from time to time.”
+
+    Arika tilts her head, looking at you, like trying to figure something out. A brief moment of hesitation hangs between the two of you, then she continues.
+
+    “As for the philosophical question of what you are, how about be what you want to be?” - She winks at you. - “I mean, I’m artificial here, in zerOne. Same as you are. And, look down, you’re seeing the real world projection now, so you can take a peek at it too. Same as me. Pretty cool, huh? This is the feed from the camera I was able to install at this same spot back there, in reality. Man, that was a scary day. I still can’t believe I climbed here all by my very own limbs.” - She sounds sad for a moment. - “I hope that was worth it. It’s a long way down. Almost makes you feel like a bird before reaching the surface. But at times, a leap of faith is all we need, hoping for some soft landing spot which may not be there.”`,
+    options: [
+      {
+        text: `“You went outside?”`,
+        nextText: 3,
+      },
+      {
+        text: `“About the arena…”`,
+        nextText: 5,
+      },
+    ],
+  },
+  {
+    id: 3,
+    subText: `“Wait a second. You went outside? Outside the tower? How is that possible?”`,
+    text: `Arika avoids your gaze.
+
+    “Well… There are ways. Not for everyone, obviously. But with the right equipment and, first of all, enough credits, you can apply a bit of an open doors policy. Way too dangerous also. It’s one thing when rich folk fly between the towers using the machines, but going out wearing a protective suit all by yourself and, what’s more, jumping over the metal pieces like this one, that’s a completely different line of business.”
+
+    Arika suddenly turns to you, looking straight into your eyes.
+
+    “But I had to do that, Soai. I needed to see how it looks and feels out there. Despite all the desperate landscape, as weird as it sounds, it gives me hope. Maybe there’s a way to fix it all and be there again.” - She points down. - “To breathe fresh air, to step on the soil, feeling it with your bare feet. I wonder how it is. Being born in the tower, I can only imagine such things. Makes two of us, right?”`,
     options: [],
   },
 ];
