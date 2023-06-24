fb1a89b0203a4372e019502e058c87cca900e8a5
Dmitrii <dimterion59@gmail.com>
2023-06-24 10:53:04 +0200

    add readme & link to the prev story

---
+++ README.md
@@ -0,0 +1,9 @@
+# Poets of Tomorrow’s World - gameOn(e)
+
+Interactive Text Adventure
+
+The next chapter of the story which began [here](https://github.com/Dimterion/PoTW)
+
+## Details
+
+Work in progress
--- src/pages/StartScreen.jsx
+++ src/pages/StartScreen.jsx
@@ -6,6 +6,15 @@ function StartScreen() {
       <h1>Poets of Tomorrow’s World - gameOn(e)</h1>
       <h2>Interactive Text Adventure</h2>
       <p>Description + reference to the previous story.</p>
+      <a
+        href="https://poets-of-tomorrows-world.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Previous Story
+      </a>
+      <br></br>
+      <br></br>
       <Link to="/story">Start</Link>
     </section>
   );
