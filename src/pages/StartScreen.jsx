a585e02d77b506d497b8f2277a54951d3d314aed
Dmitrii <dimterion59@gmail.com>
2023-06-28 10:36:02 +0200

    minor text/link correction

--- src/pages/StartScreen.jsx
+++ src/pages/StartScreen.jsx
@@ -5,16 +5,17 @@ function StartScreen() {
     <section>
       <h1>Poets of Tomorrow’s World - gameOn(e)</h1>
       <h2>Interactive Text Adventure</h2>
-      <p>Description + reference to the previous story.</p>
-      <a
-        href="https://poets-of-tomorrows-world.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Previous Story
-      </a>
-      <br></br>
-      <br></br>
+      <p>
+        The next chapter of the story which began{" "}
+        <a
+          href="https://poets-of-tomorrows-world.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+        .
+      </p>
       <Link to="/story">Start</Link>
     </section>
   );
