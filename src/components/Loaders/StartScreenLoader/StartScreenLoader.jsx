1f2b1ca53323343f3c12d0fa9ed5e59196464873
Dmitrii <dimterion59@gmail.com>
2023-07-18 14:06:09 +0200

    add start screen loader

---
+++ src/components/Loaders/StartScreenLoader/StartScreenLoader.jsx
@@ -0,0 +1,7 @@
+import "./startScreenLoader.css";
+
+function StartScreenLoader() {
+  return <span className="startScreenLoader"></span>;
+}
+
+export default StartScreenLoader;
--- src/pages/StartScreen.jsx
+++ src/pages/StartScreen.jsx
@@ -1,24 +1,43 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import StartScreenLoader from "../components/Loaders/StartScreenLoader/StartScreenLoader";
 
 function StartScreen() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1700);
+  }, []);
+
   return (
     <section className="startScreen-section">
-      <ul className="startScreen-section-slides">
-        <li className="startScreen-section-slides-slide"></li>
-        <li className="startScreen-section-slides-slide"></li>
-        <li className="startScreen-section-slides-slide"></li>
-      </ul>
-      <h1 className="startScreen-section-title">Poets of Tomorrow’s World:</h1>
-      <h1 className="startScreen-section-subTitle">gameOn(e) {"{"}</h1>
-      <div className="startScreen-section-buttons">
-        <Link className="startScreen-section-buttons-btn" to="/story">
-          START
-        </Link>
-        <Link className="startScreen-section-buttons-btn" to="/about">
-          ABOUT
-        </Link>
-      </div>
-      <h1 className="startScreen-section-closingTitle">{"}"}</h1>
+      {loading ? (
+        <StartScreenLoader />
+      ) : (
+        <>
+          <ul className="startScreen-section-slides">
+            <li className="startScreen-section-slides-slide"></li>
+            <li className="startScreen-section-slides-slide"></li>
+            <li className="startScreen-section-slides-slide"></li>
+          </ul>
+          <h1 className="startScreen-section-title">
+            Poets of Tomorrow’s World:
+          </h1>
+          <h1 className="startScreen-section-subTitle">gameOn(e) {"{"}</h1>
+          <div className="startScreen-section-buttons">
+            <Link className="startScreen-section-buttons-btn" to="/story">
+              START
+            </Link>
+            <Link className="startScreen-section-buttons-btn" to="/about">
+              ABOUT
+            </Link>
+          </div>
+          <h1 className="startScreen-section-closingTitle">{"}"}</h1>
+        </>
+      )}
     </section>
   );
 }
