7b5f827ed70de1461989df7b4858fb8ace2562a5
Dmitrii <dimterion59@gmail.com>
2023-07-19 16:16:38 +0200

    add menu for the story screen

--- src/pages/StoryScreen/StoryScreen.jsx
+++ src/pages/StoryScreen/StoryScreen.jsx
@@ -2,11 +2,37 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import storyScreenImage from "../../assets/images/story_screen_image.jpg";
 import { storyScreenMainText } from "../../utils/additionalTexts";
+import { storyScreenSubText } from "../../utils/additionalTexts";
 import { storyScreenDialogText } from "../../utils/additionalTexts";
 import "./storyScreen.css";
 
 function StoryScreen() {
-  const [dialog, setDialog] = useState(false);
+  const [state, setState] = useState({
+    dialog: false,
+    mainText: false,
+    subText: false,
+  });
+
+  function dialog() {
+    setState((prevState) => ({
+      ...prevState,
+      dialog: !state.dialog,
+    }));
+  }
+
+  function mainText() {
+    setState((prevState) => ({
+      ...prevState,
+      mainText: !state.mainText,
+    }));
+  }
+
+  function subText() {
+    setState((prevState) => ({
+      ...prevState,
+      subText: !state.subText,
+    }));
+  }
 
   return (
     <section className="storyScreen-section">
@@ -19,16 +45,16 @@ function StoryScreen() {
         The story you know so far...
       </h2>
       <article className="storyScreen-section-article">
-        <button
-          className="storyScreen-section-article-btn"
-          onClick={() => setDialog(!dialog)}
-        >
+        <button className="storyScreen-section-article-btn" onClick={dialog}>
           &#10033;
         </button>
-        <dialog className="storyScreen-section-article-dialog" open={dialog}>
+        <dialog
+          className="storyScreen-section-article-dialog"
+          open={state.dialog}
+        >
           <button
             className="storyScreen-section-article-dialog-xBtn"
-            onClick={() => setDialog(false)}
+            onClick={dialog}
           >
             &#10006;
           </button>
@@ -47,14 +73,37 @@ function StoryScreen() {
           </div>
           <button
             className="storyScreen-section-article-dialog-btn"
-            onClick={() => setDialog(false)}
+            onClick={dialog}
           >
             CLOSE
           </button>
         </dialog>
-        <p className="storyScreen-section-article-paragraph">
-          {storyScreenMainText}
-        </p>
+        <br></br>
+        <br></br>
+        <button
+          className="storyScreen-section-article-menuBtn"
+          onClick={mainText}
+        >
+          WORLD STATE
+        </button>
+        {state.mainText && (
+          <p className="storyScreen-section-article-paragraph">
+            {storyScreenMainText}
+          </p>
+        )}
+        <br></br>
+        <br></br>
+        <button
+          className="storyScreen-section-article-menuBtn"
+          onClick={subText}
+        >
+          WHO YOU ARE
+        </button>
+        {state.subText && (
+          <p className="storyScreen-section-article-paragraph">
+            {storyScreenSubText}
+          </p>
+        )}
       </article>
       <h2 className="storyScreen-section-title">What will you do now, poet?</h2>
       <Link to="/game">Jump in</Link>
