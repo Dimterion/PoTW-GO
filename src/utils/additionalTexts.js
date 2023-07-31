c3f1e01d545d33c909a2842d44d7e7775a7e95d8
Dmitrii <dimterion59@gmail.com>
2023-07-31 10:54:26 +0200

    adjust intro text

--- src/utils/additionalTexts.js
+++ src/utils/additionalTexts.js
@@ -1,32 +1,34 @@
-export const storyScreenIntroText = `Oh, hi. You clicked me. Nice!
+export const storyScreenIntroText = `Oh, hi. You’ve clicked me. Nice!
 
-So, listen, a couple of clarifications before we start. This is sort of a continuation of the story I’ve made before. I’ve no idea if you read it or not, thus allow me to point out a few things.
+So, listen, a couple of clarifications before we start, ok?
 
-If you’ve read through the previous story, first of all, thanks! Second of all, the current story takes place right after the end of the previous one. Also, I guess this may not be obvious, but this particular “end” that leads to this story is the one where you see “The End” words. Not sure if this is important, but here it is (just in case you’ve made different choices along the way back there). Hence, if you’ve reached “The End” there, you can skip the rest of the text on this page, unless you need a refresher. “Jump in” button will lead you to the beginning of the current story.
+This is a continuation of the story I’ve written before. I’ve no idea if you’ve read it or not, thus allow me to point out a few things.
 
-If you haven’t reached the end/haven’t read the previous story/etc. you can check the rest of the text on this page: first part is pretty much the same description of the world that is given for the initial story and the second one is a short version of the events that took place there.
+If you’ve read through the previous story, first of all, thanks! Second of all, the current one takes place right after it. The particular “end” that leads to this story is the one where you see “The End” words (in case you’ve made different choices along the way back there). Hence, if you’ve reached “The End” there, you can skip the rest of this page, unless you need a refresher. “Jump in” button will lead you to the beginning of the current story.
 
-I’m trying to make this one better, but want to stick to a similar structure, although who knows what it’ll look like at the end. Thing is, I just want to tell a story that has been sitting in me for many years. It may be good or bad or just ok-ish. Of that I’ve no idea, and, to be honest, I can’t say that I care much. It’s a strange reasoning, but the only one I have: I want to tell the story and put it somewhere. Well, I also try to become a better web developer (or just a developer if I may say so), but that remains to be seen.
+If you haven’t reached the end/haven’t read the previous story/etc, you can check the summary on this page: “World state” button will show you some general info on what’s going on, and “Who you are” will provide a brief recap of the events that took place before.
 
-Sorry for a bit of additional wording here, just thought it might be worth mentioning. In any case, if you’re reading this, wow, thank you. I really appreciate it. No idea if you’re going to continue reading or not, but everyone’s time is precious, so, in any case, I’m grateful you’ve spent a smidge of yours here. And just in case you intend to continue, cool, thanks again! Hope you’ll like it… though, no promises, alright? Ok, it’s better if I stop talking. Cheers!`;
+If you’re still reading this, thank you again. I really appreciate it. Everyone’s time is precious, and I’m grateful you’ve spent a smidge of yours here. And just in case you intend to continue, cool! Hope you’ll like it… though, no promises, alright?
+
+Ok, it’s better if I stop talking. Cheers!`;
 
 export const storyScreenMainText = `In the distant future Earth's surface got fully toxic. The air became completely unbreathable. In a final desperate attempt to survive humans built lifescrapers - giant structures going high into the sky, the only place where it was still possible to live. They call them towers. The last attempt to save mankind which is fading away with each passing day.
 
-Money and power are the two factors that define your fate in the tower. The more you have, the higher level of the lifescraper you can reach, increasing your chance for a longer life. The closer you are to the surface, the faster you die.
+Money and power are the two factors that define your fate in the tower. The more you have, the higher level of the lifescraper you can reach, increasing your chances for a longer life. The closer you are to the surface, the faster you die.
 
 Slowly but surely the level of pollution and toxins is growing and getting up. One day it will finally reach the top levels of the highest towers.
 
 Lifescrapers are not big enough to handle the birth rate, thus it’s heavily regulated. Movement between the towers is almost impossible. Only the richest ones have a chance to travel and visit other structures.
 
-Each tower is a separate state while all of them are being ruled by the Builders Council. Its members developed and constructed all of them long ago. Their dynasties define humans' fate since then. They occupy the top level of the main lifescraper called the Pillar.
+Each tower is a separate state while they are all ruled by the Builders Council. Its members developed and constructed them long ago. Their dynasties define humans' fate since then. They occupy the top level of the main lifescraper called the Pillar.
 
-Not having enough space, jobs and money in the real world, humans spent most of the time in the virtual one. It’s a global network called zerOne, a virtual representation of the tower you live in. It provides more space but is still restricted in terms of getting higher or lower. Your financial situation is the main factor that can remove some of the restrictions. Connections with other towers are blocked, although there are some ways to bypass it.
+Not having enough space, jobs and money in the real world, humans spend most of the time in the virtual one. It’s a global network called zerOne, a digital representation of the tower you live in. It provides more space but is still restricted in terms of getting higher or lower. Your financial situation is the main factor that can remove some of the restrictions. Connections with other towers are blocked, although there are some ways to bypass this.
 
 At the time not long before the towers era, most of the humans’ jobs had been replaced by the machines and artificial intelligence. Not having a lot of other options, the majority of the Earth’s population became coders. The profession became so popular that it led to almost full replacement of all other types of activities. Coders call themselves writers based on what they do day after day.
 
-Manual labor had ceased to exist. Builders Council controls the production of the machines and AI development. Writers form guilds that compete for each sector where their skills are still being used. Coders manage machines work, fix bugs, develop new features and compete with AI, endlessly trying to prove that they are worth it. However, the demand for them is significantly less than the population supply. Every writers’ guild is always fully staffed, and within them only the best ones manage to stay and continue working there.
+Manual labor had ceased to exist. Builders Council controls the production of the machines and AI development. Writers form guilds that compete for each sector where their skills are still being used. Coders manage machines work, fix bugs, develop new features and compete with AI, endlessly trying to prove that they are worth it. However, demand for them is significantly lower than supply. Each guild is always fully staffed, and within them only the best ones manage to stay and continue working there.
 
-Those who were not able to join the guilds search for any other source of income in zerOne. The most popular ones are Writers Fights, arenas where coders create and control artificial characters during the match. These characters fight each other in a special virtual space. Participants call themselves poets: for the majority every fight is the final frontier between the lower and higher tower’s levels. They call it the Last Song: the fight after which you either go down or rise up.`;
+Those who were not able to join the guilds search for any other source of income in zerOne. The most popular one is taking part in the Writers Fights, arenas where coders create and control artificial characters that fight each other in a special virtual space. Participants call themselves poets: for the majority every fight is the final frontier between lower and higher tower’s levels. They call it the Last Song: the fight after which you either go down or rise up.`;
 
 export const storyScreenSubText = `You are Soai, one of the poets. It wasn’t your choice to become one, it was a necessity.
 
