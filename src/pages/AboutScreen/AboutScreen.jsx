a18528842d2dbfbfa5c983da9f9db0554113404f
Dmitrii <dimterion59@gmail.com>
2023-10-18 15:32:13 +0200

    add about scr img & adjust elements

--- src/pages/AboutScreen/AboutScreen.jsx
+++ src/pages/AboutScreen/AboutScreen.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AboutScreenLoader from "../../components/Loaders/AboutScreenLoader/AboutScreenLoader";
 import structureDiagram from "../../assets/PoTW - Game_On(e)-flowchart.pdf";
+import aboutScrImg from "../../assets/images/about_screen_image.jpg";
 import "./aboutScreen.css";
 
 function AboutScreen() {
@@ -21,27 +22,28 @@ function AboutScreen() {
         <>
           <h1 className="aboutScreen-title">Poets of Tomorrow’s World:</h1>
           <h1 className="aboutScreen-subTitle">gameOn(e)</h1>
+          <img
+            src={aboutScrImg}
+            alt="A person looking at the futuristic towers."
+            className="aboutScreen-img"
+          />
           <h2 className="aboutScreen-additionalTitle">
             Interactive Text Adventure
           </h2>
           <article className="aboutScreen-mainContent">
-            <h3>
-              The next chapter of the story which began{" "}
-              <a
-                href="https://poets-of-tomorrows-world.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="aboutScreen-link"
-              >
-                here
-              </a>
-              .
-            </h3>
-            <h3 className="aboutScreen-infoTitle">WORK IN PROGRESS</h3>
-            <i>
-              {"("}but can be completed now{")"}
-            </i>
+            <i>Next chapter of Tomorrow’s World story</i>
+            <h3 className="aboutScreen-infoTitle">Details</h3>
             <ul className="aboutScreen-links">
+              <li>
+                <a
+                  href="https://poets-of-tomorrows-world.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="aboutScreen-link"
+                >
+                  First chapter of the story
+                </a>
+              </li>
               <li>
                 <a
                   href="https://medium.com/@dimterion/making-a-text-adventure-game-7e43df823f80"
@@ -72,6 +74,7 @@ function AboutScreen() {
                   Additional info
                 </a>
               </li>
+              <li>To be continued...</li>
             </ul>
             <Link className="aboutScreen-startScreenLink" to="/">
               START SCREEN
